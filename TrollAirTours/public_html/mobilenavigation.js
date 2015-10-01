780eb96a79c547708ddd6b4767bbc5a570f01f46
Eirik Nesje <ebnesjen@gmail.com>
2015-10-01 21:23:41 +0200

    BG
    Added "change BG" function for index

--- TrollAirTours/public_html/mobilenavigation.js
+++ TrollAirTours/public_html/mobilenavigation.js
@@ -40,8 +40,20 @@
        var f = document.getElementById(id2);
        var g = document.getElementById(id3);
        var h = document.getElementById(id4);
+       var bg = document.getElementById("main-top");
        e.style.display = 'block';
        f.style.display = 'none';
        g.style.display = 'none';
        h.style.display = 'none';
+       if(id == 'briksdalen-info')
+          document.getElementById('main-top').style.backgroundImage = "url('style/briksdalen.png')";
+       if(id == 'geiranger-info')
+          document.getElementById('main-top').style.backgroundImage = "url('style/geiranger5.png')";
+       if(id == 'Aakneset-info')
+          document.getElementById('main-top').style.backgroundImage = "url('style/bolgen.png')";
+       if(id == 'egendefinert-info')
+          document.getElementById('main-top').style.backgroundImage = "url('style/geiranger.png')";
+  
+             
+         
   }   
